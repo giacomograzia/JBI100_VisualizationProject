cdda13105c604e063c36e032ed91a01017ceae77
puckkwaspen <p.c.j.kwaspen@student.tue.nl>
2023-11-25 12:35:35 +0100

    update puck 25/11/2023

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from dash import html
 import plotly.express as px
 from dash.dependencies import Input, Output
 
+# THIS IS PUCK TRYING HER BEST TO WORK WITH GITHUB
+who_is_awesome = "Puck"
 
 if __name__ == '__main__':
     # Create data
